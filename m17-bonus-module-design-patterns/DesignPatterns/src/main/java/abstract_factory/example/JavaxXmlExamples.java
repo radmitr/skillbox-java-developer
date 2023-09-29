4f302b0624fc93cf63c1b6a3c920d4a81cb42287
Dmitriy Radionov <dmitriy.radionov@gmail.com>
2023-09-29 14:05:43 +0300

    Add 'JavaxXmlExamples.java' to 'abstract_factory.example'

---
+++ m17-bonus-module-design-patterns/DesignPatterns/src/main/java/abstract_factory/example/JavaxXmlExamples.java
@@ -0,0 +1,10 @@
+package abstract_factory.example;
+
+public class JavaxXmlExamples {
+
+    public static void main(String[] args) {
+        javax.xml.parsers.DocumentBuilderFactory.newInstance();
+        javax.xml.transform.TransformerFactory.newInstance();
+        javax.xml.xpath.XPathFactory.newInstance();
+    }
+}
