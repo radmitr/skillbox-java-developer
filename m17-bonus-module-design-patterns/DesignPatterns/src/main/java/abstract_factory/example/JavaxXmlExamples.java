b794d8f0eb7cc6b3d1721a4d3fe2c090110f3680
Dmitriy Radionov <dmitriy.radionov@gmail.com>
2023-10-02 10:47:47 +0300

    Add code to the abstract factory example

--- m17-bonus-module-design-patterns/DesignPatterns/src/main/java/abstract_factory/example/JavaxXmlExamples.java
+++ m17-bonus-module-design-patterns/DesignPatterns/src/main/java/abstract_factory/example/JavaxXmlExamples.java
@@ -3,8 +3,21 @@ package abstract_factory.example;
 public class JavaxXmlExamples {
 
     public static void main(String[] args) {
-        javax.xml.parsers.DocumentBuilderFactory.newInstance();
+        // 1
+        javax.xml.parsers.DocumentBuilderFactory factory1 =
+                javax.xml.parsers.DocumentBuilderFactory.newInstance();
+
+        String provider =
+                "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl";
+
+        // create a new DocumentBuilderFactory from an implementation
+        javax.xml.parsers.DocumentBuilderFactory factory2 =
+                javax.xml.parsers.DocumentBuilderFactory.newInstance(provider, null);
+
+        // 2
         javax.xml.transform.TransformerFactory.newInstance();
+
+        // 3
         javax.xml.xpath.XPathFactory.newInstance();
     }
 }
