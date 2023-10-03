f6735341b809c817934ba2082c40b3b6424ca72b
Dmitriy Radionov <dmitriy.radionov@gmail.com>
2023-10-03 17:04:54 +0300

    Add adapter main code

--- m17-bonus-module-design-patterns/DesignPatterns/src/main/java/adapter/AdapterTest.java
+++ m17-bonus-module-design-patterns/DesignPatterns/src/main/java/adapter/AdapterTest.java
@@ -1,9 +1,37 @@
 package adapter;
 
+import java.util.Date;
+import java.util.List;
+
 public class AdapterTest {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Service
+        Customer customerService = new Customer();
+        // Adapter
+        CustomerDataObject adapter = new CustomerDataObject(customerService);
 
-  }
+        // Client
+        double callsPerMonth = adapter.getValue("callsPerMonth");
+        double callsPerYear = adapter.getValue("callsPerYear");
+        double minutesPerMonth = adapter.getValue("minutesPerMonth");
+        System.out.println("callsPerMonth" + callsPerMonth);
+        System.out.println("callsPerYear" + callsPerYear);
+        System.out.println("minutesPerMonth" + minutesPerMonth);
 
+        //-----------------------------------------------------------------------
+
+        // Service examples
+        Date now = new Date();
+        Date someTimeAgo = new Date(now.getTime() - 1000);
+        int callsCount = customerService.getCallsCount(now, someTimeAgo);
+        System.out.println("callsCount: " + callsCount);
+
+        List<Call> calls = customerService.getCalls(now, someTimeAgo);
+        int minutes = 0;
+        for (Call call : calls) {
+            minutes += call.getDuration();
+        }
+        System.out.println("minutes: " + minutes);
+    }
 }
